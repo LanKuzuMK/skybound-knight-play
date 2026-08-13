3657c0a97c721843d4467cbb64204c8a7cd1d40f
LanKuzuMK <lku199776@gmail.com>
2026-08-13 09:07:51 +0000

    Serve original Sky Lore artwork publicly

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -76,7 +76,7 @@ type World = {
 const ART = {
   menu: "/manus-storage/skybound-menu-sky_f3f3d42c.jpg",
   sky: "/manus-storage/skybound-game-sky_36ed0a53.jpg",
-  castle: "/manus-storage/skybound-original-floating-castle_5484f10f.webp",
+  castle: "https://files.manuscdn.com/user_upload_by_module/session_file/310519663888325164/hjlKWDLxJbOnVdOc.webp",
   mark: "/manus-storage/skybound-compass-mark_04c7d693.png",
 };
 
